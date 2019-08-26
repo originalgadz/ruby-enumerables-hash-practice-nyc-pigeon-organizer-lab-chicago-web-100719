6bf91a68d5ddce27ec97445fb8af50165c70db0c
Glenn A Doherty <gadz@earthlink.net>
2019-08-26 19:28:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute_cat_key, attribute_hash|
+    attribute_hash.each do |attribute_key, name_array|
+      name_array.length.times do |index|
+        if pigeon_list[name_array[index]]
+          if pigeon_list[name_array[index]][attribute_cat_key] 
+            attribute=pigeon_list[name_array[index]][attribute_cat_key]
+            attribute.push(attribute_key.to_s)
+            pigeon_list[name_array[index]][attribute_cat_key]=attribute
+          else
+            attribute = []
+            attribute.push(attribute_key.to_s)
+            pigeon_list[name_array[index]][attribute_cat_key]=attribute
+          end
+        else  
+          attribute = []
+          attribute.push(attribute_key.to_s)
+          pigeon_list[name_array[index]] = {attribute_cat_key => attribute}
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
